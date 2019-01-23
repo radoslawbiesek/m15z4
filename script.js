a35c6180d6b6b75caea497207ad3eb5b9bf6077c
Radosław Biesek <radoslaw.biesek@gmail.com>
2019-01-23 22:01:12 +0100

    Solve exercises

---
+++ script.js
@@ -0,0 +1,22 @@
+// Exercise 1
+const a = 'Hello';
+const b = 'World';
+
+console.log(`${a}, ${b}!`);
+
+// Exercise 2
+const multiply = (a, b = 1) => a * b;
+
+// Exercise 3
+const average = (...nums) => { 
+    let sum = 0;
+    nums.forEach(num => sum += num);
+    return sum / nums.length;
+};
+
+// Exercise 4
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+average(...grades);
+
+// Exercise 5
+const c = [1, 4, 'Iwona', false, 'Nowak'];
